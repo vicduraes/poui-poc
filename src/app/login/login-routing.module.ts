d4c0bdb33e48b57e5c5c93e9d9d5df7d64a16762
vicduraes <duraes.vic@gmail.com>
2020-09-17 17:14:50 -0300

    chore: change login template

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-import {LoginTemplateComponent} from './login-template/login-template.component'
+import { LoginTemplateComponent } from './login-template/login-template.component'
+import { SignUpComponent } from './sign-up/sign-up.component'
 
 const routes: Routes = [
-  {path:'', component: LoginTemplateComponent}
-];
+    { path: '', component: LoginTemplateComponent },
+    { path: 'signup', component: SignUpComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class LoginRoutingModule { }
+export class LoginRoutingModule {}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
 
-import { LoginRoutingModule } from './login-routing.module';
-import { SharedModule } from '../shared/shared.module';
-import { LoginTemplateComponent } from './login-template/login-template.component';
+import { PoModule, PoI18nConfig, PoI18nModule } from '@po-ui/ng-components'
+import { LoginRoutingModule } from './login-routing.module'
+import { SharedModule } from '../shared/shared.module'
+import { LoginTemplateComponent } from './login-template/login-template.component'
+import { SignUpComponent } from './sign-up/sign-up.component'
 
+const humanResourcesPtLiterals = {
+    loginErrorPattern: 'ID inválido',
+    loginPlaceholder: 'Insira seu ID',
+    passwordErrorPattern: 'Senha incorreta',
+    passwordPlaceholder: 'Insira sua senha',
+    submitLabel: 'Acesse a sua conta',
+    forgotPassword: 'Esqueceu seu ID ou sua senha?',
+    highlightInfo: 'Para nós o futuro é agora',
+}
 
+const poI18nConfig: PoI18nConfig = {
+    contexts: {
+        general: {
+            pt: humanResourcesPtLiterals,
+        },
+    },
+    default: {
+        context: 'general',
+        cache: true,
+    },
+}
 
 @NgModule({
-  declarations: [LoginTemplateComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    LoginRoutingModule,
-  ]
+    imports: [
+        HttpClientModule,
+        SharedModule,
+        LoginRoutingModule,
+        PoModule,
+        PoI18nModule.config(poI18nConfig),
+    ],
+    declarations: [LoginTemplateComponent, SignUpComponent],
+    exports: [],
+    providers: [],
 })
-export class LoginModule { }
+export class LoginModule {}
