d6b18a47abccf00bb3c8bb57a0a9bebd7ad7c76d
vicduraes <duraes.vic@gmail.com>
2020-09-17 17:14:32 -0300

    chore: add prettier configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
 const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('../app/login/login.module').then((m) => m.LoginModule),
-  },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-];
+    {
+        path: 'login',
+        loadChildren: () =>
+            import('../app/login/login.module').then((m) => m.LoginModule),
+    },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
